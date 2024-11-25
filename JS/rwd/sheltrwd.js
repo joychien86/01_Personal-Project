33b199584c7657adc8daa2da8dc8c9f9ee6a5efe
joychien86 <joy860618@gmail.com>
2024-11-26 00:11:20 +0800

    品種介紹RWD 完成~~(共8頁)

---
+++ JS/rwd/sheltrwd.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.owl-carousel').owlCarousel({
+        items: 1,               // 每次顯示一張圖片
+        loop: true,             // 循環播放
+        autoplay: true,         // 自動播放
+        autoplayTimeout: 3000,  // 自動播放時間間隔（毫秒）
+        autoplayHoverPause: true, // 滑鼠懸停暫停
+        dots: true,             // 啟用點導航
+        nav: false              // 禁用左右箭頭
+    });
+});
